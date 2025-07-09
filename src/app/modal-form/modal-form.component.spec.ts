52f800ed16e6d130c8d0bcbe1668984eb6048388
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-07-09 10:25:19 +0500

    Update modal-form.component.spec.ts

--- src/app/modal-form/modal-form.component.spec.ts
+++ src/app/modal-form/modal-form.component.spec.ts
@@ -1,12 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-
-
-
-
-
-
-
 import { ModalFormComponent } from './modal-form.component';
 
 describe('ModalFormComponent', () => {
