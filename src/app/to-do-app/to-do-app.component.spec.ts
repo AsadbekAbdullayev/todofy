a275de0a78b97e425041b3a32ab3888ce199dfe9
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-07-27 12:13:50 +0500

    Update to-do-app.component.spec.ts

--- src/app/to-do-app/to-do-app.component.spec.ts
+++ src/app/to-do-app/to-do-app.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToDoAppComponent } from './to-do-app.component';
 
+
 describe('ToDoAppComponent', () => {
   let component: ToDoAppComponent;
   let fixture: ComponentFixture<ToDoAppComponent>;
