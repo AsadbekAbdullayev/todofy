becbf9d8d31cefd2bc51b3d5a0ceb090ce0da6e1
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-07-27 12:14:24 +0500

    Update to-do-app.component.spec.ts

--- src/app/to-do-app/to-do-app.component.spec.ts
+++ src/app/to-do-app/to-do-app.component.spec.ts
@@ -9,6 +9,7 @@ import { ToDoAppComponent } from './to-do-app.component';
 
 
 
+
 describe('ToDoAppComponent', () => {
   let component: ToDoAppComponent;
   let fixture: ComponentFixture<ToDoAppComponent>;
